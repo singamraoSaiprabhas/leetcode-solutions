d6958d581cfa001a8b85e87442f830dd8db1fb1c
Singamrao Sai prabhas <singamraosaiprabhas@gmail.com>
2026-08-01 19:03:36 +0530

    Time: 0 ms (100%), Space: 19.2 MB (79.49%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,9 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = nums[:]
+        
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
+        return dp[-1] >= 0
